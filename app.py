33d86375a81d85135cde1bbd31a816aab73f11d4
André Grandier <kennethiskandaraldrich@gmail.com>
2023-05-13 01:02:34 +0700

    FEAT: ADDING TABLE UNDER THE PREDICTION TO ITERATE THE DETAIL OF THE EPOCH MODEL

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import numpy as np
 from flask import Flask, request, render_template
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import Perceptron
@@ -27,13 +28,75 @@ X_train, X_test, y_train, y_test = train_test_split(X, labels, test_size=0.2, ra
 model = Perceptron()
 model.fit(X_train, y_train)
 
+def activation(x):
+    return 1 / (1 + np.exp(-x))
+
+def activation_derivative(x):
+    return activation(x) * (1 - activation(x))
+
+def calculate_loss(output, target):
+    return (output - target) ** 2
+
+def calculate_accuracy(predictions, targets):
+    correct_predictions = [1 if p == t else 0 for (p, t) in zip(predictions, targets)]
+    accuracy = sum(correct_predictions) / len(targets)
+    return accuracy
+
+def train(text, alpha=0.01, num_epochs=10):
+    global epochs
+    
+    x = vectorizer.transform([text])
+    w = model.coef_[0]
+    b = model.intercept_[0]
+    
+    epochs = []
+    
+    for epoch in range(num_epochs):
+        # Forward pass
+        net = x.dot(w) + b
+        output = activation(net)
+        target = 1 if model.classes_[0] == 'pos' else -1
+        loss = calculate_loss(output, target)
+        accuracy = calculate_accuracy(model.predict(x), [target])
+
+        # Backward pass
+        d_loss_d_output = output - target
+        d_output_d_net = activation_derivative(net)
+        d_loss_d_net = d_loss_d_output * d_output_d_net
+        d_net_d_w = x.toarray()[0]
+        d_net_d_b = 1
+
+        d_loss_d_w = d_loss_d_net * d_net_d_w
+        d_loss_d_b = d_loss_d_net * d_net_d_b
+
+        # Update weights and bias
+        w = w - alpha * d_loss_d_w
+        b = b - alpha * d_loss_d_b
+
+        # Store epoch information
+        epoch_info = {
+            'epoch': epoch+1,
+            'input': x.toarray()[0],
+            'weights': np.round(w, 4),
+            'target': target,
+            'bias': np.round(b[0], 4),
+            'net': net[0],
+            'output': np.round(output[0], 4),
+            'loss': np.round(loss, 4),
+            'accuracy': np.round(accuracy, 4)
+        }
+        epochs.append(epoch_info)
+
+    return epochs
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         text = request.form['text']
+        epochs = train(text)
         prediction = model.predict(vectorizer.transform([text]))[0]
         prediction = "Positive" if prediction == "pos" else "Negative"
-        return render_template('index.html', prediction=prediction, text=text)
+        return render_template('index.html', prediction=prediction, text=text, epochs=epochs)
     return render_template('index.html')
 
 if __name__ == '__main__':
